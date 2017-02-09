37c1738c4b72667f10ae34519e85d307df2b0e1e
Matthias Bachmann <M.Bachmann@itbgmbh.ch>
2017-02-09 13:09:55 +0100

    Initial project

---
+++ 10-spring-boot-starter/src/main/java/edu/spring/starter10/DemoApplication.java
@@ -0,0 +1,32 @@
+package edu.spring.starter10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+// Generated code from spring starter
+//@SpringBootApplication
+//public class DemoApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(DemoApplication.class, args);
+//	}
+//}
+
+
+@EnableAutoConfiguration
+@Controller
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World";
+	}
+	
+}
