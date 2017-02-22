914d9422275052bee294b4ffab703308701c67cd
Matthias Bachmann <M.Bachmann@itbgmbh.ch>
2017-02-22 09:23:53 +0100

    added spring boot mvc static

---
+++ 17-spring-boot-mvc-static/src/test/java/edu/spring/mvc/_static/SpringMvcApplicationTests.java
@@ -0,0 +1,20 @@
+package edu.spring.mvc._static;
+
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = SpringMvcApplication.class)
+@WebAppConfiguration
+public class SpringMvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
