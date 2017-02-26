a522633f46d2d6ed30df8df8a598571e0309bb49
Matthias Bachmann <M.Bachmann@itbgmbh.ch>
2017-02-26 18:59:16 +0100

    update Test und package names

--- 10-spring-boot-starter/src/test/java/edu/spring/starter/DemoApplicationTests.java
+++ 10-spring-boot-starter/src/test/java/edu/spring/starter/DemoApplicationTests.java
@@ -1,12 +1,14 @@
-package edu.spring.starter10;
+package edu.spring.starter;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest (classes = DemoApplication.class)
 public class DemoApplicationTests {
 
 	@Test
