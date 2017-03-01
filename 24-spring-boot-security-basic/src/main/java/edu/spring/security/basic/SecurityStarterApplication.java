6baea8d0910041cf2f422710072264fdc0b6fffa
Matthias Bachmann <M.Bachmann@itbgmbh.ch>
2017-03-01 16:48:53 +0100

    added spring security basic

---
+++ 24-spring-boot-security-basic/src/main/java/edu/spring/security/basic/SecurityStarterApplication.java
@@ -0,0 +1,14 @@
+package edu.spring.security.basic;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityStarterApplication.class, args);
+    }
+}
