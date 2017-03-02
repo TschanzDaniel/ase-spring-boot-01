2d1abf713ac86c51d7c47d2c8930d840465478fb
Matthias Bachmann <M.Bachmann@itbgmbh.ch>
2017-03-02 11:21:10 +0100

    update the config if i18n

---
+++ 18-spring-boot-mvc-i18n/src/main/java/edu/spring/mvc/i18n/config/InternationalizationConfig.java
@@ -0,0 +1,36 @@
+package edu.spring.mvc.i18n.config;
+
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class InternationalizationConfig extends WebMvcConfigurerAdapter  {
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		CookieLocaleResolver resolver = new CookieLocaleResolver();
+		resolver.setDefaultLocale(Locale.ENGLISH);
+		return resolver;
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
+}
+
+
