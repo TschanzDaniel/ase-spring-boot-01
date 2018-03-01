3ec784c24c246e3497f3ce749af5ff0321db2a2b
mbach <mbach@192.168.77.25>
2018-03-01 14:50:50 +0100

    Update 2018

--- 15-spring-boot-mongodb/src/main/java/edu/spring/mongodb/MongoDbApplication.java
+++ 15-spring-boot-mongodb/src/main/java/edu/spring/mongodb/MongoDbApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * curl -X POST -H "Content-Type: application/json" -d '{"firstname": "Matthias", "lastname":"Bachmann"}' http://localhost:8080/customers
+ * curl -X GET http://localhost:8080/customers 
+ *
+ */
+
 @SpringBootApplication
 @EnableAutoConfiguration
 public class MongoDbApplication {
